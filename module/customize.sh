51c2b896ed7b4e5ceb6baf4ec662b733b7f21161
Dr Accesorios RD <ariellourdes52@gmail.com>
2026-09-14 13:58:30 -0400

    Add meta-overlayfs replacement rule

---
+++ module/customize.sh
@@ -0,0 +1,7 @@
+REPLACE="
+/system/product/app/LatinIMEGooglePrebuilt
+"
+
+ui_print "- AlphaDroid Gboard Fix for Xiaomi Mi 9 SE (grus)"
+ui_print "- Replacing LatinIMEGooglePrebuilt systemlessly"
+ui_print "- Original /product files remain untouched"
